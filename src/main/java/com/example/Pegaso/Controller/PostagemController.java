02007169dbd65b488f0e380da4e221d88a67997a
Marcus <markbons159@gmail.com>
2022-11-14 22:03:14 -0300

    Mecanismo CORS adicionado globalmente

--- src/main/java/com/example/Pegaso/Controller/PostagemController.java
+++ src/main/java/com/example/Pegaso/Controller/PostagemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = {"http://localhost:8080","https://ifgoiano.edu.br"})
 @RestController
 @RequestMapping("Postagem/v1")
 @Tag(name = "Posts", description = "Endpoints for Managing Posts")
@@ -83,6 +84,7 @@ public class PostagemController {
         @ApiResponse(description = "Not Found",     responseCode= "404", content =  @Content),
         @ApiResponse(description = "InternalError", responseCode= "500", content =  @Content),
     })
+
     public ResponseEntity<Object> getOnePost(@PathVariable(value = "id") Long id){
        
 
