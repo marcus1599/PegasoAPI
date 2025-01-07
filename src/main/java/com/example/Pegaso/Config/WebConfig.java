e2f2aca8ce6ec04b2d412b356710ec5a6cb9e892
Marcus <markbons159@gmail.com>
2025-01-07 12:40:39 -0300

    alterando permissões no WebConfig

--- src/main/java/com/example/Pegaso/Config/WebConfig.java
+++ src/main/java/com/example/Pegaso/Config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Permite apenas seu frontend local
+                .allowedOrigins("http://localhost:4200,https://front-pegasus.vercel.app/") // Permite apenas seu frontend local e deployado
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Necessário para permitir envio de cookies ou credenciais
